ab24898da01900edbb60a390ef521f51cbd272de
upszot <upszot@gmail.com>
2023-03-27 13:04:14 -0300

    Update sed_file.sh

--- sed_file.sh
+++ sed_file.sh
@@ -4,4 +4,9 @@ FILE=$1
 IMAGEN_DOCKER = $2
 VERSION_DOCKER = $3
           
+
+echo 
+echo "FILE: $FILE"
+echo "Imagen:  ${IMAGEN_DOCKER}:${VERSION_DOCKER}"
+echo 
 sed "s/image:.*$/image: ${IMAGEN_DOCKER}:${VERSION_DOCKER}/"  $FILE
