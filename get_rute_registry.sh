db650639ffcf7e5895c4f21d5d3ce14b6f5e997f
gferrer-fusion <126263508+gferrer-fusion@users.noreply.github.com>
2023-03-30 17:22:39 -0300

    Update get_rute_registry.sh

--- get_rute_registry.sh
+++ get_rute_registry.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-OPENSHIFT_REGISTRY=$( oc get route default-route -n openshift-image-registry --template='{{ .spec.host }}')
+oc get route default-route -n openshift-image-registry --template='{{ .spec.host }}'
