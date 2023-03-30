945918f911c483acaa77ad97aec3a1e4e8541cc9
upszot <upszot@gmail.com>
2023-03-30 17:21:02 -0300

    Update get_rute_registry.sh

--- get_rute_registry.sh
+++ get_rute_registry.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-$OPENSHIFT_REGISTRY=$( oc get route default-route -n openshift-image-registry --template='{{ .spec.host }}')
+OPENSHIFT_REGISTRY=$( oc get route default-route -n openshift-image-registry --template='{{ .spec.host }}')
